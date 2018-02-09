90f3af0e9c18621dfab5422ad6daa0933a9424a2
Teh Sunn Liu (AlexLiu) <tehsunnliu@gmail.com>
2018-02-09 17:28:29 +0800

    Updated type_to_string.h

--- common/type_to_string.h
+++ common/type_to_string.h
@@ -8,13 +8,16 @@
 /* This must match the type_to_string_ cases. */
 union printable_types {
 	const struct pubkey *pubkey;
-	const struct bitcoin_txid *bitcoin_txid;
-	const struct bitcoin_blkid *bitcoin_blkid;
+	//const struct bitcoin_txid *bitcoin_txid;
+	const struct btcnano_txid *btcnano_txid;
+	//const struct bitcoin_blkid *bitcoin_blkid;
+	const struct btcnano_blkid *btcnano_blkid;
 	const struct sha256 *sha256;
 	const struct ripemd160 *ripemd160;
 	const struct rel_locktime *rel_locktime;
 	const struct abs_locktime *abs_locktime;
-	const struct bitcoin_tx *bitcoin_tx;
+	//const struct bitcoin_tx *bitcoin_tx;
+	const struct btcnano_tx *btcnano_tx;
 	const struct htlc *htlc;
 	const struct preimage *preimage;
 	const struct channel_state *channel_state;
