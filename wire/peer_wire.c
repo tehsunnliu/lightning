2eef9019fee40161edd79917760d80a81be567ab
Teh Sunn Liu (AlexLiu) <tehsunnliu@gmail.com>
2018-02-23 14:38:24 +0800

    Updated btcnano reference

--- wire/peer_wire.c
+++ wire/peer_wire.c
@@ -82,7 +82,7 @@ bool extract_channel_id(const u8 *in_pkt, struct channel_id *channel_id)
 	u16 ignored_u16;
 	u8 ignored_u8;
 	struct pubkey ignored_pubkey;
-	struct bitcoin_blkid ignored_chainhash;
+	struct btcnano_blkid ignored_chainhash;
 
 	if (fromwire_channel_reestablish(in_pkt, NULL, channel_id,
 					 &ignored_u64, &ignored_u64))
