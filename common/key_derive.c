62e4688cfb7745004214d7f74f471a6cf49ef1d9
Teh Sunn Liu (AlexLiu) <tehsunnliu@gmail.com>
2018-02-23 14:33:59 +0800

    Updated btcnano reference

--- common/key_derive.c
+++ common/key_derive.c
@@ -1,5 +1,5 @@
-#include <bitcoin/privkey.h>
-#include <bitcoin/pubkey.h>
+#include <btcnano/privkey.h>
+#include <btcnano/pubkey.h>
 #include <ccan/crypto/sha256/sha256.h>
 #include <common/key_derive.h>
 #include <common/utils.h>
